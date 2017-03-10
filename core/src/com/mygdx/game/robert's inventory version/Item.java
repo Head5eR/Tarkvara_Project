689a06426effa355d18510473d07583eabf93670
FireFoxG <gleb.rimmel@gmail.com>
2017-03-10 19:13:57 +0200

    cleaning up

---
+++ core/src/com/mygdx/game/robert's inventory version/Item.java
@@ -0,0 +1,19 @@
+package tarkvaraproject;
+
+import java.awt.image.BufferedImage;
+
+public class Item {
+	public String name;
+	public BufferedImage texture;
+	public ItemType itemType;
+	
+	public Item(String name, BufferedImage texture, ItemType itemType) {
+		this.name = name;
+		this.texture = texture;
+		this.itemType = itemType;
+	}
+
+	public ItemType getItemType() {
+		return itemType;
+	}	
+}
