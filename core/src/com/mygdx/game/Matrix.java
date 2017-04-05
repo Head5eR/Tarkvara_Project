dfb9590d5803e558c2969310aa98c31a0629ba41
FireFoxG <gleb.rimmel@gmail.com>
2017-04-06 00:45:03 +0300

    gradle support v2.0, fixed slots, equipment, inventory

--- core/src/com/mygdx/game/Matrix.java
+++ core/src/com/mygdx/game/Matrix.java
@@ -5,14 +5,14 @@ import java.util.ArrayList;
 public class Matrix {
 	private int length;
 	private int width;
-	private ArrayList<ArrayList> columns = new ArrayList<>();
+	private ArrayList<ArrayList<Integer>> columns = new ArrayList<ArrayList<Integer>>();
 	
 	public Matrix(int length, int width, int cellValues) {
 		this.setLength(length);
 		this.setWidth(width);
 		
 		for(int i=0; i<length; i++) {
-			columns.add(new ArrayList<>());
+			columns.add(new ArrayList<Integer>());
 			for(int j=0; j<width; j++) {
 				 (columns.get(i)).add(cellValues);
 				
@@ -26,7 +26,7 @@ public class Matrix {
 		this.setWidth(width);
 		
 		for(int i=0; i<length; i++) {
-			columns.add(new ArrayList<>());
+			columns.add(new ArrayList<Integer>());
 			for(int j=0; j<width; j++) {
 				 (columns.get(i)).add(15); // 15 means 1 + 2 + 4 + 8 => first 4 bits, each of them is value of the wall => 01111, 5th bit is for "visited" flag
 				
@@ -35,13 +35,15 @@ public class Matrix {
 		//System.out.println(columns.toString());
 	}
 	
-	public ArrayList<ArrayList> getMatrix(){
+	public ArrayList<ArrayList<Integer>> getMatrix(){
 		return columns;
 	}
 	
 	public int getCell(Location loc) {
 		//System.out.println("returned value: " + (int) columns.get(loc.getX()).get(loc.getY()));
+		
 		return (int) columns.get(loc.getX()).get(loc.getY());
+		//return (int) columns.get(loc.getX()).get(loc.getY());
 	}
 	
 	public int getCell(int x, int y) {
