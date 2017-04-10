c54638f2d7b9ab309a64fe17ad3f236aa040f303
eightyears <banzho12@gmail.com>
2017-04-11 01:58:37 +0300

    Commit by Alex

--- core/Items/com/mygdx/game/Bodyarmor.java
+++ core/Items/com/mygdx/game/Bodyarmor.java
@@ -1,10 +1,24 @@
 package com.mygdx.game;
 
+import java.util.List;
+
 public class Bodyarmor extends Armor {
 
-	public Bodyarmor() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
-	// stat modifiers for bodyarmor
+	 public static Bodyarmor getBodyarmorRandRarity(String level) {
+	  String type = "body";
+	  List <String> armorStats = getArmorFromXML(type, level);
+	  String name = armorStats.get(0);
+	  int armor = Integer.parseInt(armorStats.get(1));
+	  int strength = Integer.parseInt(armorStats.get(2));
+	  int dexterity = Integer.parseInt(armorStats.get(3));
+	  int stamina = Integer.parseInt(armorStats.get(4));
+	  int wrath = Integer.parseInt(armorStats.get(5));
+	  
+	  return new Bodyarmor(strength, dexterity, stamina, wrath, type, level, name, armor);
+	 }
+	 
+	 private Bodyarmor(int str, int dex, int stam, int wrath, String type, String level, String name, int armor) {
+	  super(str, dex, stam, wrath, type, level, name, armor);
+	  
+	 }
 }
