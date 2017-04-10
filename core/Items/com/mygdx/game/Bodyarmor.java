0f9c3228ae8d981debf52e240ec46b0b82bc5db0
eightyears <banzho12@gmail.com>
2017-04-11 01:48:23 +0300

    Ambush System skeleton

--- core/Items/com/mygdx/game/Bodyarmor.java
+++ core/Items/com/mygdx/game/Bodyarmor.java
@@ -1,19 +1,9 @@
 package com.mygdx.game;
 
-public class Bodyarmor extends Item {
+public class Bodyarmor extends Armor {
 
 	public Bodyarmor() {
 		super();
-		// TODO Auto-generated constructor stub
-	}
-
-	public Bodyarmor(String name, boolean equipable) {
-		super(name, equipable);
-		// TODO Auto-generated constructor stub
-	}
-
-	public Bodyarmor(String name, int rarity, boolean equipable) {
-		super(name, rarity, equipable);
 		// TODO Auto-generated constructor stub
 	}
 	// stat modifiers for bodyarmor
---
+++ core/src/com/mygdx/game/AmbushSystem.java
@@ -0,0 +1,40 @@
+package com.mygdx.game;
+
+public class AmbushSystem {
+	private double basicChance = 0.1;
+	private int length;
+	private int width;
+	private int xloc;
+	private int yloc;
+	
+	public distanceFromEntrance(MapGenerator mapG) { //insert map length, width and start location (entrance)
+		this.length = maplength; // return distance from entrance
+	}
+	
+	public generateAttackChance() {  //insert hero location
+		// return chance of being punished, slapped hard
+	}
+	
+	public double getBasicChance() {
+		return basicChance;
+	}
+
+	public int getLength() {
+		return length;
+	}
+
+	public int getWidth() {
+		return width;
+	}
+
+	public int getXloc() {
+		return xloc;
+	}
+
+	public int getYloc() {
+		return yloc;
+	}
+	
+	
+	
+}
